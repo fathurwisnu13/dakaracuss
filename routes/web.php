7381570b60f6e09119f12034a653d0fb196896eb
fathurwisnu13 <fathurwisnu13@gmail.com>
2023-08-15 16:01:53 +0700

    reusable layout pagehome-login-signup

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('login', function () {
+    return view('pages.auth.login');
+});
+
+Route::get('sign-up', function () {
+    return view('pages.auth.sign-up');
+});
